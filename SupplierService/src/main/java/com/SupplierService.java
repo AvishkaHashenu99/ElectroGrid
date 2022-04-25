f8d05f2a3082b16317306b700b8db3281a3f3b4f
ChiranjayaChandrasena <chiranjayachandrasena@gmail.com>
2022-04-25 23:28:52 +0530

    Updated the CRUD functions and implemented JUnit testcases

--- SupplierService/src/main/java/com/SupplierService.java
+++ SupplierService/src/main/java/com/SupplierService.java
@@ -14,19 +14,36 @@ import org.jsoup.*;
 import org.jsoup.parser.*; 
 import org.jsoup.nodes.Document; 
 
-	@Path("/Supplier") 
+@Path("/Supplier") 
 
 public class SupplierService {
 	
-		Supplier supplierObj = new Supplier(); 
+	  Supplier supplierObj = new Supplier(); 
 	  
-	  //view supplier
+	  //view all suppliers
 	  @GET 
 	  @Path("/") 
 	  @Produces(MediaType.TEXT_HTML) 
 	  public String readSuppliers() 
 	  { 
 	    return supplierObj.readSuppliers(); 
+	  }
+	  
+	  
+	  //view suppliers by ID
+	  @GET
+	  @Path("/supplierByID")
+	  @Consumes(MediaType.APPLICATION_JSON)
+	  @Produces(MediaType.TEXT_PLAIN)
+	  public String readSupplierByID(String powerSupplierIDData)
+	  {
+		//Convert the input string to a JSON object 
+		JsonObject supplierObject = new JsonParser().parse(powerSupplierIDData).getAsJsonObject();
+		
+		//Read the values from the JSON object
+		String powerSupplierId = supplierObject.get("powerSupplierID").getAsString();
+		
+		return supplierObj.readSupplierByID(powerSupplierId);				  
 	  }
 	  
 	  
@@ -45,7 +62,9 @@ public class SupplierService {
 		  String address = supplierObject.get("address").getAsString();
 		  String NIC = supplierObject.get("NIC").getAsString();
 		  String phone = supplierObject.get("phone").getAsString();
+		  
 		  String output = supplierObj.insertSupplier(name, address, NIC, phone);
+		  
 		  return output;
 	  }
 	  
@@ -67,7 +86,7 @@ public class SupplierService {
 	    String NIC = supplierObject.get("NIC").getAsString(); 
 	    String phone = supplierObject.get("phone").getAsString(); 
 	   
-	         String output = supplierObj.updateSupplier(powerSupplierID, name, address, NIC, phone); 
+	    String output = supplierObj.updateSupplier(powerSupplierID, name, address, NIC, phone); 
 	   
 	    return output; 
 	  } 
@@ -76,34 +95,19 @@ public class SupplierService {
 	  //delete supplier
 	  @DELETE 
 	  @Path("/") 
-	  @Consumes(MediaType.APPLICATION_XML) 
+	  @Consumes(MediaType.APPLICATION_JSON) 
 	  @Produces(MediaType.TEXT_PLAIN) 
-	  public String deleteSupplier(String supplierData) 
-	  { 
-	    //Convert the input string to an XML document 
-	    Document doc = Jsoup.parse(supplierData, "", Parser.xmlParser()); 
-	       
-	    //Read the value from the element <itemID> 
-	    String powerSupplierID = doc.select("powerSupplierID").text(); 
-	   
-	    String output = supplierObj.deleteSupplier(powerSupplierID); 
-	   
-	    return output; 
-	  }
-	  
-	  
-	  //view suppliers by ID
-	  @GET
-	  @Path("/supplierByID")
-	  @Consumes(MediaType.APPLICATION_JSON)
-	  @Produces(MediaType.TEXT_PLAIN)
-	  public String readSupplierByID(String powerSupplierIDData)
+	  public String deleteSupplier(String supplierData)
 	  {
-		//Convert the input string to a JSON object 
-		JsonObject supplierObject = new JsonParser().parse(powerSupplierIDData).getAsJsonObject();
-		//Read the values from the JSON object
-		String powerSupplierId = supplierObject.get("powerSupplierID").getAsString();
-		return supplierObj.readSupplierByID(powerSupplierId);				  
+			//Convert the input string to a JSON object 
+			JsonObject supplierObject = new JsonParser().parse(supplierData).getAsJsonObject();
+			
+			//Read the values from the JSON object
+			String powerSupplierID = supplierObject.get("powerSupplierID").getAsString();
+			
+			String output = supplierObj.deleteSupplier(powerSupplierID);
+			
+			return output;
 	  }
 
 }
